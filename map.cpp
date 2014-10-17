ac513a9fcd26c436eb295c9f2fcca66e719a7ac9
vanessalopez <vanessll@usc.edu>
2014-10-17 12:38:26 -0700

    changed add function

--- map.cpp
+++ map.cpp
@@ -1,64 +1,80 @@
-#include "map.h"
+//#include "map.h"
 #include <iostream>
 
 
 //constructor
-template <class keyType, class valueType> Map<keyType, valueType>::Map()
+template <class keyType, class valueType> 
+Map<keyType, valueType>::Map()
 {
+  //initiate head and tail to NULL
   head=NULL;
   tail=NULL;
-
-  size = 0;
+  //set the count or size of the list=0
+  count = 0;
 }
 
 
 //copy constructor
-template <class keyType, class valueType> Map<keyType, valueType>::Map (const Map<keyType, valueType> & other);
+template <class keyType, class valueType> Map<keyType, valueType>::Map (const Map<keyType, valueType> & other)
 {
-  MapItem<keyType, valueType> *copy = new MapItem<keyType, valueType>();
-  
-  other->head = this->head;
-  other->tail = this->tail;
-  //make deep copy here
+  //ititiate head and tail to NULL
+  head=NULL;
+  tail=NULL;
 
+  //create MapItem temp to go add the value of other 
+  MapItem<keyType, valueType> *temp;
+
+  //set temp to point to the head of other
+  //go through the list and add the elements into this
+  temp=other.head;
+  while(temp!=NULL){
+    add(temp->key, temp->value);
+    temp=temp->next;
+  } 
 }
 
 
 //deconstructor
 template <class keyType, class valueType> Map<keyType, valueType>::~Map()
 {
-  MapItem<keyType, valueType> *iter = new MapItem<keyType, valueType>();
-  iter=head->next;
+  //set iter to point to the head of the list
+  //go through list and erase the values
+  MapItem<keyType, valueType> *iter;
+  iter=head;
   while (head!=NULL){
+    iter=head->next;
     delete head;
     head=iter;
-    iter=head->next;
-    size--;
   }
-    //or
-  /*
-  head->prev=temp;
-  temp->next=head;
-    while(head!=NULL){
-      delete temp;
-      temp=head
-      head=head->next;
-    }
-  */
 }
 
 template <class keyType, class valueType> 
 Map<keyType, valueType>& Map<keyType, valueType>::operator= (const Map<keyType, valueType>& other){
-  this->size=other->key;
-  this->key=other->key
-  this->value=other->value;
+  if (this!=&other){
+    //this-pointer to address an &other gets the memory address of the value and compares
+    MapItem<keyType, valueType> *iter;
+    iter=this->head;
+    while(iter!=NULL){
+      iter=head->next;
+      delete head;
+      head=iter;
+    }
+    iter=other.head;
+    count=0;
+    while(iter!=NULL){
+      add(iter->key, iter->value);
+      iter=iter->next;    
+    }
+  }
+    return *this;
 }
+
 
 // returns the number of key-value pairs
 template <class keyType, class valueType>
 int Map<keyType, valueType>::size () const
 {  
-  return size;
+  return this->count;
 }
 
 
@@ -70,37 +86,50 @@ If the key already has an association, it should do nothing.
 //if key exists and already has a value then do nothing
 //if key doesnt exist add it and assign the value to key
 template <class keyType, class valueType>
-void Map<keyType, valueType>::add (const key, &value)
+void Map<keyType, valueType>::add (const keyType& key, valueType& value)
 {
   MapItem<keyType, valueType> *newitem = new MapItem<keyType, valueType>();
-
-  if (head=NULL){
-    head=newitem;
-    tail=newitem;
+  newitem=head;
+  //check if the list is empty
+  //if it is then just add the key and value
+  if (head==NULL){
     newitem->key=key;
     newitem->value=value;
-    size++;
+    this->count++;
   } 
-  else if (this->key == key){
-    if (this->value == NULL){
-      tail->next=newitem;
-      newitem->prev=tail;
-      tail=newitem;
-      tail->next=NULL;
-      newitem->value = value;
-  }
-    else{
-      cout << "You already have a value associated to this key!";
-  }
-  }
   else{
-      tail->next=newitem;
+  //goes through list
+    while(head!=NULL){
+      //if they key is already in the list:
+        while(newitem->key!=key);
+        newitem=newitem->next;
+        }
+      //newitem is now pointing to the key, but if the key is in the list then the newitm is point to that location
+      if (newitem->key == key){
+        //then check if the key has a value associate to it
+        if (newitem->value == NULL){
+          //if it doesnt have one associated to it then add value
+          newitem->value=value;
+          tail->next=newitem;
+          newitem->prev=tail;
+          tail=newitem;
+          tail->next=NULL;
+          newitem->value = value;
+        }
+        //if it already has a value associated to it then do nothing
+        else{
+          std::cout << "You already have a value associated to this key!" << std::endl;
+        }
+      }
+      else{
+      newitem=tail->next;
       newitem->prev=tail;
       tail=newitem;
       tail->next=NULL;
       newitem->key=key;
       newitem->value=value;
-      size++;
+      this->count++;
+      }
   }
 }
 
@@ -110,16 +139,16 @@ If the key has no association, it should do nothing. */
 //checks for key
 //erases the value
 template <class keyType, class valueType>
-void Map<keyType, valueType>::remove (const key)
+void Map<keyType, valueType>::remove (const keyType key)
 {
+  MapItem<keyType, valueType> *iter;
   //if the list is empty throw exeption
-  if (head==NULL)
+  if (head==NULL){
     throw NoSuchElementException();
+  }
   //if the list is not empty and has a key associated
   else if (this->key==key){
     //means that we found the place where the key matches
-  MapItem<keyType, valueType> *iter = new MapItem<keyType, valueType>();
-    
     iter=head;
     //iter->next=head->next;
 
@@ -129,16 +158,13 @@ void Map<keyType, valueType>::remove (const key)
     }
     //iter is now pointing to the the place to the key we want to remove
     (iter->prev)->next = iter->next;
-    delete iter;
 
-    size --;
-
+    this->count--;
+  } 
   //means that there is no association with the key
   else{
-      cout << "There is no association for that key";
-    } 
-  }
-  
+      std::cout << "There is no association for that key" << std::endl;
+  }  
 }
 
 
@@ -152,9 +178,9 @@ if you do the valueType then you have to change your add function
 if you do the const valueType then you have to modify what is in that function 
 to make it to pointers to add your information because it is const*/
 template <class keyType, class valueType>
-const valueType Map<keyType, valueType>::& get (const keyType & key) const
+const valueType& Map<keyType, valueType>::get (const keyType & key) const
 {
- MapItem<keyType, valueType> *iter = new MapItem<keyType, valueType>();
+ MapItem<keyType, valueType> *iter;
 
  iter=head; 
 
@@ -168,24 +194,27 @@ const valueType Map<keyType, valueType>::& get (const keyType & key) const
 }
 
 
+
      /* Adds all the key-value associations of other to the current map.
         If both maps (this and other) contain an association for the same
         key, then the one of this is used. */
      // get the function of other
     //check if what is in other in in map if not add that into map.
 template <class keyType, class valueType>
-void Map::merge (const Map<keyType, valueType> & other){
-MapItem<keyType, valueType> *iter = new MapItem<keyType, valueType>();
-  iter=other.head
-        if (other->key==this->key){
-          if(this->value==NULL){
-            add(other);
-          }
-          else{
-          cout << "Key already has a value associated to it." << endl;
-          }
+void Map<keyType, valueType>::merge (const Map<keyType, valueType> & other){
+MapItem<keyType, valueType> *otherIter;
+  otherIter=other.head;
+MapItem<keyType, valueType> *iter;
+  iter=this.head;
+      while (iter->next!=NULL){
+        if (iter->key==this->key && iter->value!=this->value){
+          std::cout << "There is already an assoication with the key:" << iter->key << std::endl;
+          iter=iter->next;
         }
         else{
-          add(other);
+        add(iter->key, iter->value);
+        iter=iter->next;
         }
+      }
+      add(iter->key, iter->value);
 }
